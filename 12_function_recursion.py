3abb7fbfcad11f067103ce7f29182ba1b7e15857
kannu-blip <kannu777a@gmail.com>
2026-01-05 15:05:05 +0000

    recursion

--- 12_function_recursion.py
+++ 12_function_recursion.py
@@ -11,12 +11,23 @@ def avg():
 # avg()     #Function call
 # avg()
 
-def gd(name, ending):
+def gd(name, ending = "thanks"):
     print("Good Day! " + name)
     print(ending)
     return "done"
 gd("kinoo", "Thankyouu")
-gd("harry", "thnkyouuu")
+gd("harry")
 
 a = gd("kannu", "Thankyou")
 print(a)
+
+# RECURSION - it is a function which calls itself.
+
+def factorial(n):
+    if(n==0 or n==1):
+        return 1
+    return n * factorial(n-1)
+
+n = int(input("Enter the number: "))
+print(f"The factorial of this number is: {factorial(n)}")
+
